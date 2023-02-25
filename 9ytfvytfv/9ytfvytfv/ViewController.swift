affe29e3260e8eacc3256c7f192f11d2ba3b3876
ToshpolatovXayriddin <ddargon53@gmail.com>
2023-02-25 21:39:59 +0500

    -firstCommit

---
+++ 9ytfvytfv/9ytfvytfv/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  9ytfvytfv
+//
+//  Created by MacBook Pro on 25/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
